57a38ba58f620fa4fe92262a326fe8be51b4e72a
terencech <terence000@gmail.com>
2022-04-11 13:40:39 -0700

    add remaining crud functions

--- server/controllers/mismate-controller.js
+++ server/controllers/mismate-controller.js
@@ -16,9 +16,27 @@ exports.createMismate = async (req, res) => {
 
 exports.readMismates = async (req, res) => {
   MismateModel.find({
-    locationId: null
+    userId: null
   }, (err, mismates) => {
     if (err) res.json(err);
     res.json(mismates);
+  });
+}
+
+exports.updateMismate = async (req, res) => {
+  const id = req.body.id;
+
+  MismateModel.findByIdAndUpdate(id, (err, mismate) => {
+    if (err) res.json(err);
+    res.json(mismate);
+  });
+}
+
+exports.deleteMismate = async (req, res) => {
+  const id = req.body.id;
+
+  MismateModel.findByIdAndDelete(id, (err, mismate) => {
+    if (err) res.json(err);
+    res.json(mismate);
   })
 }
