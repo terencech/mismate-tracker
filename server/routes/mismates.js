6b9b2796bb040dc9aee49b0c27db2c7f956eece1
terencech <terence000@gmail.com>
2022-04-11 13:38:44 -0700

    rename routes to follow crud convention

--- server/routes/mismates.js
+++ server/routes/mismates.js
@@ -1,9 +1,13 @@
 const { validateMismate } = require('../middleware/validation/mismate-validation.js');
-const { createMismate, readMismates } = require('../controllers/mismate-controller.js');
+const { createMismate, readMismates, updateMismate, deleteMismate } = require('../controllers/mismate-controller.js');
+const verifyToken = require('../middleware/auth.js');
 
 function mismateRoutes(app) {
-  app.route('/mismates/new').post(validateMismate, createMismate);
+  app.use(verifyToken);
+  app.route('/mismates').post(validateMismate, createMismate);
   app.route('/mismates').get(readMismates);
+  app.route('/mismates').put(updateMismate);
+  app.route('/mismates').delete(deleteMismate);
 }
 
 module.exports = mismateRoutes;
