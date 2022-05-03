0c67b3c25eb07a5c4bb10ada9d23eb344da5d067
terencech <terence000@gmail.com>
2022-05-03 12:52:12 -0700

    login redirects authenticated user

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,7 +1,11 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router";
 import ApiService from "../adapters/ApiService";
 
 export default function LoginForm() {
 
+  const [ isLoggedIn, setIsLoggedIn ] = useState();
+  const navigate = useNavigate();
 
   function handleLogin(e) {
     e.preventDefault();
@@ -13,10 +17,25 @@ export default function LoginForm() {
     }
 
     ApiService.post('/users/login', user, res => {
-      const token = res.data.token;
-      localStorage.setItem('token', token);
+      if (res.data.token) {
+        const token = res.data.token
+        localStorage.setItem('token', token);
+        navigate('/mismates');
+      }
     })
   }
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      navigate('/mismates');
+    } else {
+      ApiService.get('/isUserAuth', {
+        headers: { 'x-access-token': localStorage.getItem('token') }
+      }, (res) => {
+        setIsLoggedIn(res.data.isLoggedIn);
+      });
+    }
+  }, [isLoggedIn, navigate])
 
   return (
     <form id="login-form" onSubmit={ e => handleLogin(e) }>
