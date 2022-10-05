818f8d51fa7ae0d9dc9a7895814cf7a706621d66
terencech <terence000@gmail.com>
2022-10-05 14:08:38 -0700

    edit and delete for existing matches find new matches

--- server/models/mismate.js
+++ server/models/mismate.js
@@ -3,10 +3,12 @@ const Schema = mongoose.Schema;
 
 const mismateSchema = new Schema({
   userId: { type: Schema.Types.ObjectId, ref: 'User' },
+  userName: { type: String, default: null },
   sku: { type: Number, required: true },
   side: { type: String, enum: ['right', 'left'], required: true },
   hasBox: { type: Boolean, required: true },
   matchId: { type: Schema.Types.ObjectId, ref: 'Mismate', default: null },
+  matchUser: { type: String, default: null },
   tracking: { type: String, default: null }
 });
 
